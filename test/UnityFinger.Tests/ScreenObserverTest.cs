f477b1acce25f571359ec54894a079a60f5bb9d0
Shohei Miyashita <sound39music@gmail.com>
2017-09-12 20:05:22 +0900

    add test files

---
+++ test/UnityFinger.Tests/ScreenObserverTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UnityFinger.Tests
+{
+    [TestFixture]
+    public class ScreenObserverTest
+    {
+        [Test]
+        public void Works()
+        {
+            Assert.AreEqual(5, 1 + 4);
+        }
+    }
+}
