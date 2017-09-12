746a4088fb61f32034a9c28d2ecd99b5e1118681
Shohei Miyashita <sound39music@gmail.com>
2017-09-12 17:56:50 +0900

    fix: move file

--- src/UnityFinger/IFingerObserverConfig.cs
+++ src/UnityFinger/IFingerObserverConfig.cs
@@ -19,24 +19,4 @@
         float TwoFingersTapStartDuration { get; }
         float TwoFingersTapReleaseDuration { get; }
     }
-
-    public class DefaultFingerObserverConfig : IFingerObserverConfig
-    {
-        public float DragDuration { get { return 0.15f; } }
-        public float DragDistance { get { return 0.05f; } }
-
-        public float FlickDistance { get { return 0.05f; } }
-
-        public float LongTapDuration { get { return 0.6f; } }
-        public float LongTapDistance { get { return 0.05f; } }
-
-        public float PinchStartDistance { get { return 0.02f; } }
-
-        public float TapDuration { get { return 0.15f; } }
-        public float TapDistance { get { return 0.02f; } }
-
-        public float TwoFingersTapDuration { get { return 0.05f; } }
-        public float TwoFingersTapStartDuration { get { return 0.25f; } }
-        public float TwoFingersTapReleaseDuration { get { return 0.3f; } }
-    }
 }
